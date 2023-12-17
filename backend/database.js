02e2e363488382aeb2bb022b4b77bb8d5988a9db
pandacase <2227119810@qq.com>
2023-12-18 00:23:01 +0800

    Create database.js

---
+++ backend/database.js
@@ -0,0 +1,29 @@
+
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('database:' + connection.state);
+})
+
+
+class dbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new dbService();
+    }
+
+    
+}
