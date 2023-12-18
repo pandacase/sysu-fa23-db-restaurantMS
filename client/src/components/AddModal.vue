c3dc2fe444d6cf9885fe35cbd5aad185078d65df
pandacase <2227119810@qq.com>
2023-12-18 17:01:38 +0800

    Update AddModal.vue

---
+++ client/src/components/AddModal.vue
@@ -0,0 +1,32 @@
+
+<script>
+export default {
+  name: 'AddModal'
+}
+</script>
+
+<template>
+  <div class="AddModal">
+    <div class="backgroud">
+      <div class="window"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.backgroud {
+  /* display: none; */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+}
+
+.window {
+  
+  background-color: white;
+}
+</style>
