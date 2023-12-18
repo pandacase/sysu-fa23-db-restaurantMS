d81dd9c4b4d7d01031e67188eec4508c58538290
pandacase <2227119810@qq.com>
2023-12-18 12:45:36 +0800

    add views

---
+++ server/src/views/order.js
@@ -0,0 +1,82 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService = require('../database/database');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+
+// get
+app.get('/get', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+    const result = db.getAllDataFromOrders();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// search
+app.get('/search/date', (request, response) => {
+    const { date_left } = request.body;
+    const { date_right } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(date);
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+app.get('/search/price', (request, response) => {
+    const { price_left } = request.body;
+    const { price_right } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(date);
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// create
+app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(name);
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+});
+
+// delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(id);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
+
+// update
+app.patch('/update', (request, response) => {
+    const { id } = request.body;
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(id, name);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
+
+module.exports = app;
---
+++ server/src/views/table.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService = require('../database/database');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+
+// get
+app.get('/get', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+    const result = db.getAllDataFromTables();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// create
+app.post('/insert', (request, response) => {
+    const { table_id } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(name);
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+});
+
+// delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(id);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
+
+// update
+app.patch('/update', (request, response) => {
+    const { id } = request.body;
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.TMP(id, name);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
+
+
+module.exports = app;
