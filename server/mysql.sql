57bac16d9f56ea22ccd831f8f66e09b11e70cd2b
pandacase <2227119810@qq.com>
2023-12-18 11:38:43 +0800

    Update mysql.sql

---
+++ server/mysql.sql
@@ -0,0 +1,37 @@
+
+create database restaurant;
+
+use restaurant;
+
+create table dishes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(31),
+    price FLOAT,
+    description VARCHAR(255),
+    icon BLOB
+);
+
+create table orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    time_added DATETIME,
+    item_list JSON,
+    total_price FLOAT
+);
+
+create table tables (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    table_id INT,
+    type VARCHAR(15),
+    customer_num INT,
+    CONSTRAINT ct_num CHECK (customer_num >= 0)
+);
+
+
+
+create user admin;
+
+grant all on restaurant.dishes to admin;
+grant all on restaurant.orders to admin;
+grant all on restaurant.tables to admin;
+
+
