58211988d13326a329b5c08a58e541adf49518ff
pandacase <2227119810@qq.com>
2023-12-18 12:45:50 +0800

    Create app.js

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const menu = require('./views/menu');
+const order = require('./views/order');
+const table = require('./views/table');
+
+// set the router
+app.use('/menu', menu);
+app.use('/order', order);
+app.use('/table', table);
+
+// run the app
+app.listen(process.env.PORT, () => console.log('App is running'));
