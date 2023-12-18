2a7dfb8ad13dbe37eaf08d5d9bcabecb6ee84a99
pandacase <2227119810@qq.com>
2023-12-18 15:40:57 +0800

    Create DataTable.vue

---
+++ client/src/components/DataTable.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+  <div class="dataTable">
+    <table>
+        <thead>
+        <th>Table #</th>
+        <th>Type</th>
+        <th>c</th>
+        </thead>
+        <tbody></tbody>
+    </table>
+  </div>
+
+</template>
+
+
+<style scoped>
+
+</style>
