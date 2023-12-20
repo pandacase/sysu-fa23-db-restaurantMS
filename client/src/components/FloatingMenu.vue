92840aedd57db189b0c3ed1939b8b107f6b60fbd
pandacase <2227119810@qq.com>
2023-12-20 12:18:37 +0800

    Update FloatingMenu.vue

--- client/src/components/FloatingMenu.vue
+++ client/src/components/FloatingMenu.vue
@@ -1,14 +1,33 @@
 
-<script>
-export default {
-  name: 'FloatingMenu'
-}
+<script setup>
+
 </script>
 
 <template>
-
+  <div class="FloatingMenu">
+    <div class="btnList">
+      <button id="backToTopBtn">Top</button>
+      <button id="refreshBtn">Re</button>
+      <button id="addBtn">Add</button>
+    </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
+@import url('../icons/iconfont.css');
+
+.btnList {
+  position: fixed;
+  top: 75%;
+  transform: translateY(-50%);
+  right: 100px;
+}
+
+.btnList button {
+  display: block;
+  width: 50px;
+  height: 50px;
+  margin: 20px 10px;
+}
 
 </style>
