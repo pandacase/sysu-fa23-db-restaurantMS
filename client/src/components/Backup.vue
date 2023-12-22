45f3daae42fc1ccebf4fc52a2e4e2b9cf33c4f86
pandacase <2227119810@qq.com>
2023-12-22 14:22:05 +0800

    Create Backup.vue

---
+++ client/src/components/Backup.vue
@@ -0,0 +1,29 @@
+<!-- <template #body>
+    <div v-for="col in modalContentColumns" :key="col.id">
+      <div v-if="col === 'price' || col === 'total_price' || col === 'table_id' || col === 'customer_num'">
+        <label :for="col">{{ capitalize(col) }}</label>
+        <input type="number" :id="col">
+      </div>
+      <div v-else-if="col === 'description'">
+        <label :for="col">{{ capitalize(col) }}</label>
+        <textarea :id="col" :rows="4"></textarea>
+      </div>
+      <div v-else-if="col === 'item_list'">
+        <label :for="col">{{ capitalize(col) }}</label>
+        <div :id="col">还没有放东西</div>
+      </div>
+      <div v-else-if="col === 'type'">
+        <label :for="col">{{ capitalize(col) }}</label>
+        <select v-model="selected">
+          <option disabled value="">Please select one</option>
+          <option>small(2)</option>
+          <option>middle(4)</option>
+          <option>large(8)</option>
+        </select>
+      </div>
+      <div v-else>
+        <label :for="col">{{ capitalize(col) }}</label>
+        <input type="text" :id="col">
+      </div>
+    </div>
+  </template> -->
