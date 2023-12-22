d4c0c9364f8f567ce7b1d1349df76135547111e3
pandacase <2227119810@qq.com>
2023-12-22 14:22:37 +0800

    Update OrderMSView.vue

--- client/src/views/OrderMSView.vue
+++ client/src/views/OrderMSView.vue
@@ -5,14 +5,15 @@ import DataTable from '@/components/DataTable.vue'
 import FloatingMenu from '@/components/FloatingMenu.vue'
 
 const ordersColumns = ref(['id', 'time_added', 'item_list', 'total_price'])
+const ordersColumnsToAdd = ref(['item_list', 'total_price'])
 
 const reload = ref(false)
 </script>
 
 <template>
   <div class="orderMS">
-    <FloatingMenu @reloadData="reload = true"/>
-
     <DataTable :gridColumns="ordersColumns" :reload="reload" @reloadFinished="reload = false"/>
+      
+    <FloatingMenu :gridColumnsToAdd="ordersColumnsToAdd" @reloadData="reload = true"/>
   </div>
 </template>
