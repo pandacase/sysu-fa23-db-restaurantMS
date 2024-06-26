22b29b82f1029e75de1d45e9f56ff6a7098350e4
pandacase <2227119810@qq.com>
2024-06-26 11:48:22 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@ export default {
 <template>
   <div class="home">
     <img alt="logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Restaurant Management App"/>
+    <HelloWorld msg="Welcome to Restaurant Management App👍👍👍"/>
   </div>
 </template>
 
